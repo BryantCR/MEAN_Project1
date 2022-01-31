91ebb65241ac10282015c0b5cfaa840bd7237dd1
JohandroG <johandrogonzalez@gmail.com>
2022-01-31 12:04:04 -0600

    Changes

---
+++ server/config/database.js
@@ -0,0 +1,6 @@
+var mongoose = require("mongoose");
+
+
+
+//!FOR PROD PORPOSES
+mongoose.connect("mongodb+srv://johang:201010jk@cluster0.anbzh.mongodb.net/NoticeBoard?retryWrites=true&w=majority");
