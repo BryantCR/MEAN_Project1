ae03342517a4a15122141accb20b7b7d40dd6fc0
BryantCR <bryancvargas20@gmail.com>
2022-02-02 20:58:04 -0600

    ok

--- server/routers/userRouter.js
+++ server/routers/userRouter.js
@@ -2,6 +2,7 @@ const express = require( 'express' );
 const UserRouter = express.Router();
 const {UserController}  = require('../controllers/userController');
 
-
+UserRouter
+    .post('/register', UserController.createNewUser)
 
 module.exports = { UserRouter };
