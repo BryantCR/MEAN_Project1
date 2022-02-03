a61a7ae61f100ea833e1503edefce8f410c63483
BryantCR <bryancvargas20@gmail.com>
2022-02-02 21:08:24 -0600

    Ok

--- server/config/database.js
+++ server/config/database.js
@@ -6,3 +6,12 @@ var mongoose = require("mongoose");
 
 //!FOR PROD PORPOSES
 mongoose.connect("mongodb+srv://johang:201010jk@cluster0.anbzh.mongodb.net/Company?retryWrites=true&w=majority");
+
+mongoose.connection.on('error', err => {
+    console.error(`Mongoose default connection error: ${ err }`);
+    process.exit(0);
+});
+
+mongoose.connection.on('disconnected', () => {
+    console.log('Mongoose default connection disconnected');
+});
